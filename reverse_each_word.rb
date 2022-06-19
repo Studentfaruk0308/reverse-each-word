bc4a895132230ba41068c53ae051b6543c59b5fd
faruk <studentfaruk0308@gmail.com>
2022-06-20 08:00:50 +1000

    After Completion

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+# def reverse_each_word(sentence)
+#     results = [];
+#     sentence.split.each{|word| result << word.reverse}
+#     return results.joint(" ")
+# end
+
+def reverse_each_word(sentence)
+    sentence.split.collect{|word| word.reverse}.join(" ")
+end
